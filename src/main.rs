81806cb6edc2283cafe80a475e46ad3c7ec48fca
psvm203 <psvm203@proton.me>
2025-06-09 10:36:33 +0900

    Add GitHub icon

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,8 @@ use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+        <img class={"w-64"} src={"images/markdown.png"} />
     }
 }
 
